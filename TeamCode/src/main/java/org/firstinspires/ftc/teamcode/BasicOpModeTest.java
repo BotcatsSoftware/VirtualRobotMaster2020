93f9191e38f6f74ea8ce356f77249b966047843d
BotcatSoftware <BotcatSoftware@gmail.com>
2020-09-06 13:18:40 -0700

    Updating the auto class file to move forward and include telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpModeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpModeTest.java
@@ -29,12 +29,15 @@
 
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 
 /**
@@ -50,7 +53,7 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Basic: Linear OpMode", group="Linear Opmode")
+@Autonomous(name="Basic OpMode Test", group="Linear Opmode")
 @Disabled
 public class BasicOpModeTest extends LinearOpMode {
 
@@ -61,54 +64,41 @@ public class BasicOpModeTest extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-        telemetry.addData("Status", "Initialized");
+
+        DcMotor frontLeft =hardwareMap.dcMotor.get("front_left_motor");
+        DcMotor frontRight =hardwareMap.dcMotor.get("front_right_motor");
+        DcMotor backLeft =hardwareMap.dcMotor.get("back_left_motor");
+        DcMotor backRight =hardwareMap.dcMotor.get("back_right_motor");
+
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        backRight.setDirection(DcMotor.Direction.REVERSE);
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+
+        DistanceSensor frontDistance = hardwareMap.get(DistanceSensor.class, "front_distance");
+        DistanceSensor backDistance = hardwareMap.get(DistanceSensor.class, "back_distance");
+        DistanceSensor leftDistance = hardwareMap.get(DistanceSensor.class, "left_distance");
+        DistanceSensor rightDistance = hardwareMap.get(DistanceSensor.class, "right_distance");
+
+        frontLeft.setPower(-1);
+        frontRight.setPower(-1);
+        backLeft.setPower(-1);
+        backRight.setPower(-1);
+
+        telemetry.addData("Front Distance", " %.1f", frontDistance.getDistance(DistanceUnit.CM));
+        telemetry.addData("Left Distance", " %.1f", leftDistance.getDistance(DistanceUnit.CM));
+        telemetry.addData("Right Distance", " %.1f", rightDistance.getDistance(DistanceUnit.CM));
+        telemetry.addData("Back Distance", " %.1f", backDistance.getDistance(DistanceUnit.CM));
+        telemetry.addData("Encoders"," %d %d %d %d", backLeft.getCurrentPosition(), frontLeft.getCurrentPosition(),
+                frontRight.getCurrentPosition(), backRight.getCurrentPosition());
         telemetry.update();
-
-        // Initialize the hardware variables. Note that the strings used here as parameters
-        // to 'get' must correspond to the names assigned during the robot configuration
-        // step (using the FTC Robot Controller app on the phone).
-        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
-        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
-
-        // Most robots need the motor on one side to be reversed to drive forward
-        // Reverse the motor that runs backwards when connected directly to the battery
-        leftDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightDrive.setDirection(DcMotor.Direction.REVERSE);
-
-        // Wait for the game to start (driver presses PLAY)
-        waitForStart();
-        runtime.reset();
-
-        // run until the end of the match (driver presses STOP)
-        while (opModeIsActive()) {
-
-            // Setup a variable for each drive wheel to save power level for telemetry
-            double leftPower;
-            double rightPower;
-
-            // Choose to drive using either Tank Mode, or POV Mode
-            // Comment out the method that's not used.  The default below is POV.
-
-            // POV Mode uses left stick to go forward, and right stick to turn.
-            // - This uses basic math to combine motions and is easier to drive straight.
-            double drive = -gamepad1.left_stick_y;
-            double turn  =  gamepad1.right_stick_x;
-            leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-            rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
-
-            // Tank Mode uses one stick to control each wheel.
-            // - This requires no math, but it is hard to drive forward slowly and keep straight.
-            // leftPower  = -gamepad1.left_stick_y ;
-            // rightPower = -gamepad1.right_stick_y ;
-
-            // Send calculated power to wheels
-            leftDrive.setPower(leftPower);
-            rightDrive.setPower(rightPower);
-
-            // Show the elapsed game time and wheel power.
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
-            telemetry.update();
         }
     }
-}
